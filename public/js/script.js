fa2b4a5c95e9bb33a058a983985a0e0a1a32c377
salioufereya <salioufereya19@gmail.com>
2023-05-28 22:35:18 +0200

    sauvgarde

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,13 @@
 
-
-
-
-
-
+let bntAdd = document.getElementById('btnAdd');
+let frm = document.querySelector('#frm');
+let btnClose = document.querySelector('#btnClose');
+bntAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("hello");
+    frm.style.transform = 'translateY(10%)';
+})
+btnClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    frm.style.transform = 'translateY(-550%)';
+})
